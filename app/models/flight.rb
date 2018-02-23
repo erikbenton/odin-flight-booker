4244f2471c831fd2483f7fbd59c8ebfd4899c44e
erikbenton <erik.benton@hotmail.com>
2018-02-23 11:30:19 -0500

    adding simple search form for flights

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,9 @@
 class Flight < ApplicationRecord
   belongs_to :starting_airport,  foreign_key: "starting_airport_id",	class_name: "Airport"
   belongs_to :finishing_airport, foreign_key: "finishing_airport_id", class_name: "Airport"
+
+  def departure_time_formatted
+  	departure_time.strftime("%m/%d/%Y %H:%M")
+  end
+
 end
